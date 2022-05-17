1a78d62e4a7113d3aea9921bc7498bc09317e81d
Aaron Noe <aarminoe@gmail.com>
2022-05-16 22:08:57 -0500

    Added change question function, deleted radio input IDs

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const option1 = document.getElementById('option1')
 const option2 = document.getElementById('option2')
 const option3 = document.getElementById('option3')
 const option4 = document.getElementById('option4')
+const button = document.getElementById('submit')
 
+const questionArray = ['What day is it?',]
+const option1Array = ['Friday',]
+const option2Array = ['Saturday',]
+const option3Array = ['Sunday',]
+const option4Array = ['Monday-Thursday',]
 
 function randomCocktail() {
     fetch('https:www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
@@ -12,15 +18,24 @@ function randomCocktail() {
     .then(data => randomDrink(data))
 }
 
+function changeQuestion() {
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+        let newIndex = -1
+        question.innerHTML = `2. ${questionArray[newIndex +=1]}`
+    })
+}
+
+
 function randomDrink(objArray) {
     drinkArray = Object.values(objArray)
     console.log(drinkArray)
     for (const drink of drinkArray) {
         console.log(drink)
-
+        
     }
 }
 
 
-
 randomCocktail()
+changeQuestion()
