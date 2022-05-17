d061a8fbd701a5f930eee490355444fcefc09b6c
Aaron Noe <aarminoe@gmail.com>
2022-05-16 21:16:07 -0500

    Added IDs to p element in html, grabbed variables from html in index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 let h1 = document.getElementById('h1')
+const question = document.getElementById('question')
+const option1 = document.getElementById('option1')
+const option2 = document.getElementById('option2')
+const option3 = document.getElementById('option3')
+const option4 = document.getElementById('option4')
+
 
 function randomCocktail() {
     fetch('https:www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
